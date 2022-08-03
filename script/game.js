51e6c69368a86ed3ba265458edcd166b0d6ace7b
Nigel Doheny <nigel.doheny93@gmail.com>
2022-08-03 13:46:11 +0000

    CI using the game section

--- script/game.js
+++ script/game.js
@@ -15,12 +15,19 @@ function newGame() {
 
 function addTurn() {
     game.playerMoves = [];
-    game.currentGame.push(game.choices[(Math.floor(Math.random() *4))]);
-    //showTurns();
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    // showTurns();
+}
+
+function lightsOn(circ) {
+    document.getElementById(circ).classList.add(circ + "light");
+    setTimeout(function () {
+        document.getElementById(circ).classList.remove(circ + "light");
+    }, 400);
 }
 
 function showScore() {
     document.getElementById("score").innerText = game.score;
 }
 
-module.exports = { game, newGame, showScore,addTurn };
+module.exports = { game, newGame, showScore, addTurn, lightsOn };
