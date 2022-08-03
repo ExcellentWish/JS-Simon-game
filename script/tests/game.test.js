31187d2b21ecd241e848e91d8d0eaa74f734fa03
Nigel Doheny <nigel.doheny93@gmail.com>
2022-08-03 13:31:15 +0000

    Add addturn function

--- script/tests/game.test.js
+++ script/tests/game.test.js
@@ -2,7 +2,8 @@
  * @jest-environment jsdom
  */
 
- const { game, newGame, showScore } = require("../game");
+
+const { game, newGame, showScore, addTurn} = require("../game");
 
 
  beforeAll(() => {
@@ -45,11 +46,14 @@
      test("should display 0 for the element with id of score", () => {
          expect(document.getElementById("score").innerText).toEqual(0);
      });
+     test("Should be one move in the computer's game array", () => {
+        expect(game.currentGame.length).toBe(1);
+     })
      test("should clear the player moves array", () => {
          expect(game.playerMoves.length).toBe(0);
      });
-     test("should clear the computer sequence array", () => {
-         expect(game.currentGame.length).toBe(0);
-     });
+    //  test("should clear the computer sequence array", () => {
+    //      expect(game.currentGame.length).toBe(0);
+    //  });
  });
  
