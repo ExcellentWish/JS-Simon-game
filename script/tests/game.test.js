460ce38b4d24c821cdb5ed6224be981744b4a6c8
Nigel Doheny <nigel.doheny93@gmail.com>
2022-08-03 12:56:04 +0000

    Add newgame function to pass test

--- script/tests/game.test.js
+++ script/tests/game.test.js
@@ -1,7 +1,7 @@
 /**
  * @jest-environment jsdom
  */
-const {game} = require("../game")
+const {game, newGame} = require("../game")
 
 beforeAll( () => {
     let fs = require("fs");
@@ -27,4 +27,14 @@ describe("game object contains correct keys", () => {
     test("choices contain correct ids", () => {
          expect(game.choices).toEqual(["button1","button2","button3","button4"]);
     });
-})
+});
+
+describe("New game functions works", () => {
+    beforeAll(() => {
+        game.score = 42;
+        newGame();
+    });
+    test("Should set new game function to 0", () => {
+        expect(game.score).toEqual(0);
+    });
+});
